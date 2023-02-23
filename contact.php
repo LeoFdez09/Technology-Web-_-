f78d619c479c300a93febc29284aeb1a0346245f
Leo <126182469+LeoFdez09@users.noreply.github.com>
2023-02-23 19:20:53 +0000

    contacto update

--- contact.php
+++ contact.php
@@ -1,20 +1,18 @@
 <?php
 
     $to = "2022e2afernandez.ledel@algeciras.salesianos.edu";
-    $email = $_POST["mail"];
+    $email = $_POST["email"];
     $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    $message = $_POST["textarea"];
+    $name = $_POST["nome"];
 
     $header = "Enviado desde la pagina de tecnología";
+    $mensajeCompleto = $message . "\nAtentamente:" . $name;
 
+    mail($to, $subject, $mensajeCompleto, $header);
 
-    mail($to, $subject, $message, $header);
-
-
-
-
-
-
-
+    
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\")</script>";
 
 ?>
